a508d2e7e633c27ea2836975d13a4c90eff65aa7
bitnot <bitnot01@gmail.com>
2019-01-28 11:21:40 +0100

    Exercises 4.1 - 4.8

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -3,35 +3,98 @@ package fpinscala.errorhandling
 
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
-sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
+sealed trait Either[+E, +A] {
+  // 4.6
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(b) => Right(f(b))
+  }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Right(b) => f(b)
+  }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Right(_) => this
+    case _ => b
+  }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = (this, b) match {
+    case (Right(x), Right(y)) => Right(f(x, y))
+    case (Left(x), _) => Left(x)
+    case (_, Left(x)) => Left(x)
+  }
 }
-case class Left[+E](get: E) extends Either[E,Nothing]
-case class Right[+A](get: A) extends Either[Nothing,A]
+
+case class Left[+E](get: E) extends Either[E, Nothing]
+
+case class Right[+A](get: A) extends Either[Nothing, A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  // 4.7
+  def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = es match {
+    case Nil => Right(Nil)
+    case h :: t => f(h) match {
+      case l@Left(_) => l
+      case Right(x) => traverse(t)(f).map(xs => x :: xs)
+    }
+  }
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = traverse(es)(identity)
 
-  def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
-    if (xs.isEmpty) 
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty)
       Left("mean of empty list!")
-    else 
+    else
       Right(xs.sum / xs.length)
 
-  def safeDiv(x: Int, y: Int): Either[Exception, Int] = 
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
     try Right(x / y)
-    catch { case e: Exception => Left(e) }
+    catch {
+      case e: Exception => Left(e)
+    }
 
   def Try[A](a: => A): Either[Exception, A] =
     try Right(a)
-    catch { case e: Exception => Left(e) }
+    catch {
+      case e: Exception => Left(e)
+    }
+
+}
+
+// 4.8
+object Validation {
+
+  sealed trait ValidatedList[+E, +A] {
+    def map[B](f: A => B): ValidatedList[E, B] = this match {
+      case i@Invalid(error) => i
+      case Valid(result) => Valid(f(result))
+    }
+
+    def map2[EE >: E, B, C](other: ValidatedList[EE, B])(f: (A, B) => C): ValidatedList[EE, C] = (this, other) match {
+      case (Valid(a), Valid(b)) => Valid(f(a, b))
+      case (Invalid(errorsA), Invalid(errorsB)) => Invalid(errorsA ++ errorsB)
+      case (i@Invalid(errors), _) => i
+      case (_, i@Invalid(errors)) => i
+    }
+  }
+
+  def valid[E, A](a: A): ValidatedList[E, A] = Valid(a)
+
+  def invalid[E, A](errors: List[E]): ValidatedList[E, A] = Invalid(errors)
+
+  case class Invalid[+E](errors: List[E]) extends ValidatedList[E, Nothing]
+
+  case class Valid[+A](result: A) extends ValidatedList[Nothing, A]
+
+  def main(args: Array[String]): Unit = {
+    val v1: ValidatedList[String, Int] = valid(1)
+    val v5: ValidatedList[String, Int] = valid(5)
+    val i1: ValidatedList[String, Int] = invalid(List("error1"))
+    val i2: ValidatedList[String, Int] = invalid(List("error2"))
+    println(s"... = ${v5.map2(v1)(_ + _)}")
+    println(s"... = ${v5.map2(i1)(_ + _)}")
+    println(s"... = ${i2.map2(i1)(_ + _)}")
+    println(s"... = ${i2.map2(v1)(_ + _)}")
+  }
 
 }
--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,17 +4,35 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
+  // 4.1
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(a) if (f(a)) => this
+    case _ => None
+  }
 }
+
 case class Some[+A](get: A) extends Option[A]
+
 case object None extends Option[Nothing]
 
 object Option {
@@ -24,7 +42,9 @@ object Option {
       val x = 42 + 5
       x + y
     }
-    catch { case e: Exception => 43 } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
+    catch {
+      case e: Exception => 43
+    } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
   }
 
   def failingFn2(i: Int): Int = {
@@ -32,17 +52,54 @@ object Option {
       val x = 42 + 5
       x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
     }
-    catch { case e: Exception => 43 }
+    catch {
+      case e: Exception => 43
+    }
   }
 
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  // 4.2
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
+  // 4.3
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+    case (Some(x), Some(y)) => Some(f(x, y))
+    case _ => None
+  }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  // 4.4
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case None :: _ => None
+    case Some(a) :: tail =>
+      sequence[A](tail) match {
+        case None => None
+        case Some(list) => Some(a :: list)
+      }
+  }
+
+  // 4.5
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => f(h) match {
+      case None => None
+      case Some(x) => traverse(t)(f).map(xs => x :: xs)
+    }
+  }
+  def sequenceT[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(identity)
+}
+
+object OptionTest extends App {
+  println(s"Option.sequence(List(Some(1), Some(2), Some(3))) = ${Option.sequence(List(Some(1), Some(2), Some(3)))}")
+  println(s"Option.sequence(List(Some(1), None, Some(3))) = ${Option.sequence(List(Some(1), None, Some(3)))}")
+  println(s"Option.sequence(List()) = ${Option.sequence(List())}")
+  println(s"Option.traverse[Int, Int](List(1, 2, 3))(x => if (x > 1) Some(x) else None) = ${
+    Option.traverse[Int, Int](List(1, 2, 3))(x => if (x > 1) Some(x) else None)
+  }")
+  println(s"Option.traverse[Int, Int](List(2, 3, 4))(x => if (x > 1) Some(x) else None) = ${
+    Option.traverse[Int, Int](List(2, 3, 4))(x => if (x > 1) Some(x) else None)
+  }")
 }
